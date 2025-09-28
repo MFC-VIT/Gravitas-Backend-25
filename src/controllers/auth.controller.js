9c753ebb8737e97a4aaf6b5a4a7cc3601ba30189
S Adithya Nachiyappan <adithyanachiyappan@gmail.com>
2025-09-28 13:13:11 +0530

    403 to 401 for Access Token and JWT Access Time 1500m

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -10,7 +10,7 @@ const generateAccessToken = (user) => {
   return jwt.sign(
     { id: user.id, email: user.email, role: user.role },
     process.env.JWT_SECRET,
-    { expiresIn: '15m' }
+    { expiresIn: '1500m' }
   );
 };
 
